84319ccd4e315f535cb9497b00a780c2b12a5d7d
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-07-09 23:13:10 +0100

    wrote media queries for big screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './scss/main.scss';
+
+window.onload = () => {
+  document.getElementById('app').style.display = `block`;
+}
+
+const closePortfolio = document.getElementById('close-portfolio');
+const openPortfolio = document.querySelectorAll(".open-portfolio");
+const portfolio = document.getElementById('portfolio');
+
+portfolio.addEventListener('click', function(e) {
+  if (e.target.id === 'portfolio') {
+    this.style.display = 'none';
+  }
+})
+
+openPortfolio.forEach(item => {
+  item.addEventListener('click', function () {
+    portfolio.style.display = 'grid';
+  })
+})
+
+closePortfolio.addEventListener('click', function() {
+  portfolio.style.display = 'none';
+})
